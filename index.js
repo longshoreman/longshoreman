16f8730021d6eecb349328d0ccf8cdf233b9c891
Maxime Santerre <mooxeh@gmail.com>
2014-12-19 10:19:01 -0800

    More verbose logging
     - Included the Morgan module for request logging
     - Raise an error if the specified port is already in use
     - Handle errors through the errorHandler module

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 'use strict';
 
-var http       = require('http');
-var express    = require('express');
-var debug      = require('debug')('longshoreman');
-var router     = require('./src/router');
-var controller = require('./src/controller');
-var redisCmd   = require('./src/redis').redisCmd;
+var http          = require('http');
+var express       = require('express');
+var debug         = require('debug')('longshoreman');
+var router        = require('./src/router');
+var controller    = require('./src/controller');
+var redisCmd      = require('./src/redis').redisCmd;
+var errorHandler  = require('errorhandler');
 
 http.globalAgent.maxSockets = Infinity;
 
 var PORT = process.env.PORT || 3000;
 
 var app = express();
+
+app.use(require('morgan')('dev'));
 
 app.use(function(req, res, next) {
   var host = req.get('host');
@@ -26,11 +29,18 @@ app.use(function(req, res, next) {
   }
 });
 
-app.listen(PORT, function() {
-  debug('Listening on port ' + PORT);
-});
-
 process.on('uncaughtException', function(err) {
-  debug('Caught exception: ' + err, err.stack);
+  app.use(errorHandler({ dumpExceptions: true, showStack: true }));
   process.exit(1);
 });
+
+app.listen(PORT, function() {
+  debug('Listening on port ' + PORT);
+}).on('error', function(err) {
+  if(err.code === "EADDRINUSE") {
+    console.log("Another process is already listening on port: " + PORT)
+  } else {
+    console.log(err);
+  }
+  process.exit(1);
+})
